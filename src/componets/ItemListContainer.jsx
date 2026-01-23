385dc75f7f8067109adfe5aca2b7b397af11addd
Diego Parreira <daparrei@quilmes.com.ar>
2026-01-23 11:32:15 -0300

    Agrego el cargando al API de productos

--- src/componets/ItemListContainer.jsx
+++ src/componets/ItemListContainer.jsx
@@ -3,19 +3,22 @@ import ItemList from "./ItemList";
 
 const ItemListContainer = ({ greeting }) => {
   const [products, setProducts] = useState([]);
+  const [logging, setLogging] = useState(false);
 
   useEffect(() => {
     fetch("https://69071d72b1879c890ed8d89d.mockapi.io/productos")
       .then(res => res.json())
       .then(data => setProducts(data))
-      .catch(err => console.error("Error al cargar productos:", err));
+      .catch(err => console.error("Error al cargar productos:", err))
+      .finally(() => setLogging(true));  
+     
   }, []);
 
   return (
     <div className="container mt-4">
       <h2>{greeting}</h2>
-      <ItemList products={products} />
-    </div>
+      { logging == true ? <ItemList products={products} /> : <p>Cargando productos...</p>}
+      </div>
   );
 };
 
