7882aa9249bd24f9269a4a73b0b1cd6f445b3506
Diego Parreira <daparrei@quilmes.com.ar>
2026-01-30 11:38:35 -0300

    Dejo NavBar solo

--- src/App.jsx
+++ src/App.jsx
@@ -13,26 +13,13 @@ function App() {
 
   return (
  <BrowserRouter>
-     <Routes> 
-       <Route path='/' element={
-    <div>
       <NavBar />
-      <ItemListContainer greeting={"Bienvenido a ZAPATILLAS QUILMES"} />
-    </div>} />
-      <Route path='/about' element={
-    <div>
-      <NavBar />
-      <About />
-    </div>} />
-      <Route path='/product/:id' element={
-    <div>
-      <NavBar />
-      <ProductDetail />
-    </div>} />
+      <Routes> 
+        <Route path='/' element={<ItemListContainer greeting={"Bienvenido a ZAPATILLAS QUILMES"} />} />
+        <Route path='/about' element={ <About />} />
+        <Route path='/product/:id' element={<ProductDetail />} />
      </Routes>
  </BrowserRouter>
-
-    
   )
 }
 
